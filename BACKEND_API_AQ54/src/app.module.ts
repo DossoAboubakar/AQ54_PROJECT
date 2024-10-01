1e187f3820876d260a19037c560a9246fc18a472
Aboubakar <MinPAVR_AsNbFaeuxNzQ>
2024-10-01 01:54:37 +0000

    version corrige

--- BACKEND_API_AQ54/src/app.module.ts
+++ BACKEND_API_AQ54/src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AppController } from './app.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import * as dotenv from 'dotenv';
 import { SdataByDayModule } from './sdata_by_day/sdata_by_day.module';
 import { SdataDefaultModule } from './sdata_default/module/sdata_default_module.module';
-import { SensorDataByRangeService } from './sdata_by_range/service/sdata_by_period.service';
-import { SensorDataByRangeController } from './sdata_by_range/controller/sdata_by_range.controller';
 import { SdataByRangeModule } from './sdata_by_range/sdata_by_range.module';
 import { SensorAverageDataModule } from './s_average_data/s_average_data.module';
 dotenv.config();
@@ -14,11 +12,11 @@ dotenv.config();
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DB_HOST || 'db',  // Utilisez 'db' par défaut
-      port: parseInt(process.env.DB_PORT) || 5432, // Utilisez 5432 par défaut
+      host: process.env.DB_HOST || 'db',  
+      port: parseInt(process.env.DB_PORT) || 5432, 
       username: process.env.DB_USERNAME || 'postgres',
-      password: process.env.DB_PASSWORD || 'postgres', // Assurez-vous que le mot de passe correspond
-      database: process.env.DB_NAME || 'aq54', // Assurez-vous que c'est le bon nom de base de données
+      password: process.env.DB_PASSWORD || 'postgres', 
+      database: process.env.DB_NAME || 'AQ54_DB', 
       entities: ['dist/**/*.entity{.ts,.js}'],
       synchronize: true,
     }),
@@ -26,6 +24,7 @@ dotenv.config();
     SdataDefaultModule,
     SdataByRangeModule,
     SensorAverageDataModule,
+    
   ],
   controllers: [AppController],
   providers: [AppService],
